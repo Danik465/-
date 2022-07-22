3f9c562da8e180991770cf9cf58c7d694da86f68
Danila <dan_crysis@mail.ru>
2022-07-22 22:12:31 +0300

    ООП

---
+++ Вступительный урок ООП/Program.cs
@@ -0,0 +1,34 @@
+﻿
+
+enum Color
+{
+    Red, Blue, Green, Orange, Yellow
+}
+
+class Point
+{
+    public int X;
+    public int Y;
+
+    public Color color;
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Point point = new Point();
+
+        point.X = 4;
+        point.Y = 2;
+        point.color= Color.Blue;
+
+        Point point2 = new Point();
+
+        point2.X = 4;
+        point2.Y = 2;
+
+        Console.WriteLine($"{point.X} , {point.Y} , Color {point.color}");
+       
+    }
+}
---
+++ Уроки по ООП/Урок №1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+class Student
+{
+    public Guid id;
+    public string FirstName;
+    public string LastName;
+    public string MiddleName;
+    public int age;
+    public string group;
+}
+
+
+class Program
+{
+    static Student GetStudent()
+    {
+        Student student = new Student();
+
+        student.age = 19;
+        student.FirstName = "Данила";
+        student.LastName = "Обжерин";
+        student.MiddleName = "Сергеевич";
+        student.group = "МТЭ-2-21";
+        student.id = Guid.NewGuid();
+
+        return student;
+    }
+
+    static void Print(Student student)
+    {
+        Console.WriteLine("Информация о студенте:");
+        Console.WriteLine($"1. Возраст: {student.age}");
+        Console.WriteLine($"2. Имя {student.FirstName}");
+        Console.WriteLine($"3. Фамилия {student.LastName}");
+        Console.WriteLine($"4. Отчество {student.MiddleName}");
+        Console.WriteLine($"5. Группа {student.group}");
+        Console.WriteLine($"6. id {student.id}");
+    }
+    static void Main(string[] args)
+    {
+        Student firstStudent = GetStudent();
+
+        Print(firstStudent);
+    }
+}
